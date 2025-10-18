2ec8874b78725801b6966a9719701928b84a0f6b
Chalatphon <66070041@kmitl.ac.th>
2025-10-18 17:58:03 +0700

    finish?

--- netmiko_final.py
+++ netmiko_final.py
@@ -1,34 +1,33 @@
 from netmiko import ConnectHandler
 from pprint import pprint
 
-device_ip = "<!!!REPLACEME with router IP address!!!>"
-username = "admin"
-password = "cisco"
+device_ip = "10.0.15.138"
+username = "cisco"
+password = "cisco123!"
 
 device_params = {
-    "device_type": "<!!!REPLACEME with device type for netmiko!!!>",
+    "device_type": "cisco_ios",
     "ip": device_ip,
     "username": username,
     "password": password,
 }
-
-
 def gigabit_status():
     ans = ""
     with ConnectHandler(**device_params) as ssh:
         up = 0
         down = 0
         admin_down = 0
-        result = ssh.send_command("<!!!REPLACEME with proper command!!!>", use_textfsm=True)
+        result = ssh.send_command("sh int", use_textfsm=True)
         for status in result:
-            if <!!!Write code here!!!>:
-                <!!!Write code here!!!>
-                if <!!!Write code here!!!> == "up":
+            if status["interface"].startswith("GigabitEthernet"):
+                ans += f"{status['interface']} {status['link_status']}, "
+                if status['link_status'] == "up":
                     up += 1
-                elif <!!!Write code here!!!> == "down":
+                elif status['link_status'] == "down":
                     down += 1
-                elif <!!!Write code here!!!> == "administratively down":
+                elif status['link_status'] == "administratively down":
                     admin_down += 1
-        ans = <!!!Write code here!!!>
+
+        ans = f"{ans[:-2]} -> {up} up, {down} down, {admin_down} administratively down"
         pprint(ans)
         return ans
--- restconf_final.py
+++ restconf_final.py
@@ -3,104 +3,132 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 # Router IP Address is 10.0.15.181-184
-api_url = "<!!!REPLACEME with URL of RESTCONF Configuration API!!!>"
+api_url = "https://10.0.15.138/restconf/data"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
-headers = <!!!REPLACEME with Accept and Content-Type information headers!!!>
-basicauth = ("admin", "cisco")
-
+headers =  {
+    "Accept": "application/yang-data+json",
+    "Content-Type": "application/yang-data+json",
+}
+basicauth = ("cisco", "cisco123!")
+studentID = "66070041"
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": f"Loopback{studentID}",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [{"ip": "172.30.30.1", "netmask": "255.255.255.0"}]
+            },
+            "ietf-ip:ipv6": {},
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.put(
+        f"{api_url}/ietf-interfaces:interfaces/interface=Loopback{studentID}", 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return f"Interface loopback {studentID} is created successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return f"Cannot create: Interface loopback {studentID}"
 
 
 def delete():
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
+    resp = requests.delete(
+        f"{api_url}/ietf-interfaces:interfaces/interface=Loopback{studentID}", 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return f"Interface loopback {studentID} is deleted successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return f"Cannot delete: Interface loopback {studentID}"
 
 
 def enable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": f"Loopback{studentID}",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.patch(
+        f"{api_url}/ietf-interfaces:interfaces/interface=Loopback{studentID}", 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return f"Interface loopback {studentID} is enabled successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return f"Cannot enable: Interface loopback {studentID}"
 
 
 def disable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": f"Loopback{studentID}",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": False,
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.patch(
+        f"{api_url}/ietf-interfaces:interfaces/interface=Loopback{studentID}", 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return f"Interface loopback {studentID} is shutdowned successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return f"Cannot shutdown: Interface loopback {studentID}"
 
 
 def status():
-    api_url_status = "<!!!REPLACEME with URL of RESTCONF Operational API!!!>"
-
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+    resp = requests.get(
+        f"{api_url}/ietf-interfaces:interfaces-state/interface=Loopback{studentID}",
+        auth=basicauth,
+        headers=headers,
+        verify=False,
+    )
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
         response_json = resp.json()
-        admin_status = <!!!REPLACEME!!!>
-        oper_status = <!!!REPLACEME!!!>
+        admin_status = response_json["ietf-interfaces:interface"]["admin-status"]
+        oper_status = response_json["ietf-interfaces:interface"]["oper-status"]
         if admin_status == 'up' and oper_status == 'up':
-            return "<!!!REPLACEME with proper message!!!>"
+            return f"Interface loopback {studentID} is enabled"
         elif admin_status == 'down' and oper_status == 'down':
-            return "<!!!REPLACEME with proper message!!!>"
+            return f"Interface loopback {studentID} is disabled"
     elif(resp.status_code == 404):
         print("STATUS NOT FOUND: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return f"No Interface loopback {studentID}"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
+        return f"Undefined Error"
